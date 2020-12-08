6bc3dda62bd7bca0c46c99fb80bbfe7ef2628e0d
Fred Collman <fredcollman@gmail.com>
2020-12-08 19:41:03 +0000

    Implement basic echo client

--- main.ts
+++ main.ts
@@ -1,23 +1,85 @@
 import * as log from "https://deno.land/std@0.80.0/log/mod.ts";
-import { serve, Status } from "https://deno.land/std@0.80.0/http/mod.ts";
+import {
+  serve,
+  ServerRequest,
+  Status,
+} from "https://deno.land/std@0.80.0/http/mod.ts";
+import {
+  acceptWebSocket,
+  isWebSocketCloseEvent,
+  isWebSocketPingEvent,
+  WebSocket,
+} from "https://deno.land/std@0.80.0/ws/mod.ts";
 
-const body = "Hello World\n";
-const server = serve({ port: 8000 });
+const handleWs = async (sock: WebSocket) => {
+  console.log("socket connected!");
+  try {
+    for await (const ev of sock) {
+      if (typeof ev === "string") {
+        // text message.
+        console.log("ws:Text", ev);
+        await sock.send(ev);
+      } else if (ev instanceof Uint8Array) {
+        // binary message.
+        console.log("ws:Binary", ev);
+      } else if (isWebSocketPingEvent(ev)) {
+        const [, body] = ev;
+        // ping.
+        console.log("ws:Ping", body);
+      } else if (isWebSocketCloseEvent(ev)) {
+        // close.
+        const { code, reason } = ev;
+        console.log("ws:Close", code, reason);
+      }
+    }
+  } catch (err) {
+    console.error(`failed to receive frame: ${err}`);
 
-const getResponse = async (url: string) => {
-  switch (url) {
+    if (!sock.isClosed) {
+      await sock.close(1000).catch(console.error);
+    }
+  }
+};
+
+const getResponse = async (req: ServerRequest) => {
+  switch (req.url) {
     case "/":
+    case "/index.html":
       return { body: await Deno.readFile("public/index.html") };
     case "/client.js":
       return { body: await Deno.readFile("public/client.js") };
+    case "/ws":
+      const { conn, r: bufReader, w: bufWriter, headers } = req;
+      acceptWebSocket({
+        conn,
+        bufReader,
+        bufWriter,
+        headers,
+      })
+        .then(handleWs);
+      return;
     default:
       return { body: "Not found.", status: Status.NotFound };
   }
 };
 
-for await (const req of server) {
-  log.info(`incoming: ${req.method} ${req.url}`);
-  const res = await getResponse(req.url);
-  log.info(`replying: ${res.status || Status.OK} ${req.method} ${req.url}`);
-  req.respond(res);
+const main = async () => {
+  for await (const req of serve({ port: 8000 })) {
+    const { method, url } = req;
+    log.info(`incoming: ${method} ${url}`);
+    try {
+      const res = await getResponse(req);
+      if (res) {
+        log.info(`replying: ${res.status || Status.OK} ${method} ${url}`);
+        req.respond(res);
+      }
+    } catch (err) {
+      console.error(`failed to accept websocket: ${err}`);
+      await req.respond({ status: 400 });
+    }
+  }
+};
+
+if (import.meta.main) {
+  await main();
 }
--- public/client.js
+++ public/client.js
@@ -1,4 +1,5 @@
 let username;
+let ws;
 
 const onSubmitUsername = (e) => {
   console.debug("starting onSubmitUsername");
@@ -6,14 +7,21 @@ const onSubmitUsername = (e) => {
   const submitted = e.target["username"].value;
   if (submitted && submitted.length) {
     username = submitted;
+    ws.send(`New username ${username}`);
   }
+};
+
+const onReceiveMessage = (e) => {
+  console.log(e);
 };
 
 const init = () => {
   console.log("starting init");
   const usernameForm = document.querySelector("#username");
+  ws = new WebSocket("ws://localhost:8000/ws");
 
   usernameForm.addEventListener("submit", onSubmitUsername);
+  ws.onmessage = onReceiveMessage;
 
   console.log("finishing init");
 };
