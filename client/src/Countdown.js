ba6f7dc659575cac8b4d8c13deebb3c31ec2bf58
Fred Collman <fredcollman@gmail.com>
2020-12-30 23:37:18 +0000

    Move everything inside the hook

--- client/src/Countdown.js
+++ client/src/Countdown.js
@@ -1,24 +1,15 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useReducer } from "react";
 
 const useCountdown = ({ from, onComplete }) => {
   const reducer = (state, action) => {
     switch (action.type) {
       case "TICK":
-        return { ...state, now: action.now };
-      case "COMPLETE":
-        return { ...state, completed: true };
+        const completed = action.now - state.start > 1000 * from;
+        return { ...state, now: action.now, completed };
       default:
         return state;
     }
   };
-
-  const debugReduce = (state, action) => {
-    // console.log("before reduce", state, action);
-    const result = reducer(state, action);
-    // console.log("after reduce", state, action);
-    return result;
-  };
-
   const init = () => {
     const start = new Date();
     return {
@@ -28,49 +19,31 @@ const useCountdown = ({ from, onComplete }) => {
     };
   };
 
-  const [state, dispatch] = useReducer(debugReduce, null, init);
-  return [state, dispatch];
+  const [state, dispatch] = useReducer(reducer, null, init);
+  const { start, now, completed } = state;
+  useEffect(() => {
+    const interval = window.setInterval(() => {
+      dispatch({ type: "TICK", now: new Date() });
+    }, 100);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [dispatch]);
+  useEffect(() => {
+    if (completed) {
+      console.log("completing...");
+      onComplete();
+    }
+  }, [completed, onComplete]);
+
+  const elapsed = now - start;
+  const remainingMs = 1000 * from - elapsed;
+  const remaining = Math.ceil(remainingMs / 1000);
+  return { remaining };
 };
 
 const Countdown = ({ from, onComplete }) => {
-  const [state, dispatch] = useCountdown({ from, onComplete });
-
-  const elapsed = state.now - state.start;
-  const remainingMs = 1000 * from - elapsed;
-  const remaining = Math.ceil(remainingMs / 1000);
-
-  useEffect(() => {
-    if (state.completed) {
-      // console.log("we are already done");
-      return;
-    }
-
-    const adjustRemaining = () => {
-      // console.log("adjust");
-      const now = new Date();
-      dispatch({ type: "TICK", now });
-      if (now - state.start > 1000 * from) {
-        // console.log(now, "adjustRemaining says we are done");
-        dispatch({ type: "COMPLETE", onComplete });
-      }
-    };
-
-    const interval = window.setInterval(adjustRemaining, 100);
-    // console.log("START", interval);
-    dispatch({ type: "START", interval });
-    return () => {
-      // console.log("CLEAR", interval);
-      dispatch({ type: "CLEAR", interval });
-      clearInterval(interval);
-    };
-  }, [from, state.start, state.completed, onComplete, dispatch]);
-
-  useEffect(() => {
-    if (state.completed) {
-      onComplete();
-    }
-  }, [state.completed, onComplete]);
-
+  const { remaining } = useCountdown({ from, onComplete });
   return remaining;
 };
 
