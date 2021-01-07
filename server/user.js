253f5ef8e2b65dc506b42c7623da65dc9e9ab9f8
Fred Collman <fredcollman@gmail.com>
2021-01-07 20:13:01 +0000

    Make sure the DB always writes successfully

--- server/user.js
+++ server/user.js
@@ -1,20 +1,24 @@
 import express, { Router } from "express";
 import { v4 } from "uuid";
 
+const info = (id, message) => {
+  console.log(`[user:${id}] ${message}`);
+};
+
 const makeRouter = ({ db }) => {
   const router = Router();
   router.use(express.json());
 
-  router.post("/", (req, res) => {
+  router.post("/", async (req, res) => {
     const id = v4();
     const { username } = req.body;
-    console.log(req.body);
     const user = {
       id,
       username,
     };
-    db.get("users").push(user).write();
-    res.send(user);
+    info(id, "new user signed up");
+    const data = await db.get("users").push(user).last().write();
+    res.send(data);
   });
   return router;
 };
