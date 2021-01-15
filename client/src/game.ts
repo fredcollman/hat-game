63fe1bec16c945f8782ddd8b973339456c01ad96
Fred Collman <fredcollman@gmail.com>
2021-01-15 19:05:01 +0000

    Split state into discriminating union

--- client/src/game.ts
+++ client/src/game.ts
@@ -15,15 +15,8 @@ interface TeamScore {
   skips: number;
 }
 
-type Phase =
-  | "CHOOSE_GROUP"
-  | "SIGN_UP"
-  | "CONFIGURE_GAME"
-  | "PLAY"
-  | "GAME_OVER";
-
-export interface State {
-  phase: Phase;
+interface ChooseGroup {
+  phase: "CHOOSE_GROUP";
   describer: User | null;
   round: number;
   currentSuggestion: string | null;
@@ -36,6 +29,68 @@ export interface State {
   suggestionCount: number;
   numTeams: number;
 }
+
+interface SignUp {
+  phase: "SIGN_UP";
+  describer: User | null;
+  round: number;
+  currentSuggestion: string | null;
+  turnDurationSeconds: number;
+  scores: TeamScore[];
+  groupID: string | null;
+  teams: Team[];
+  clientID: string | null;
+  users: User[];
+  suggestionCount: number;
+  numTeams: number;
+}
+
+interface ConfigureGame {
+  phase: "CONFIGURE_GAME";
+  describer: User | null;
+  round: number;
+  currentSuggestion: string | null;
+  turnDurationSeconds: number;
+  scores: TeamScore[];
+  groupID: string | null;
+  teams: Team[];
+  clientID: string | null;
+  users: User[];
+  suggestionCount: number;
+  numTeams: number;
+}
+
+interface Play {
+  phase: "PLAY";
+  describer: User | null;
+  round: number;
+  currentSuggestion: string | null;
+  turnDurationSeconds: number;
+  scores: TeamScore[];
+  groupID: string | null;
+  teams: Team[];
+  clientID: string | null;
+  users: User[];
+  suggestionCount: number;
+  numTeams: number;
+}
+
+interface GameOver {
+  phase: "GAME_OVER";
+  describer: User | null;
+  round: number;
+  currentSuggestion: string | null;
+  turnDurationSeconds: number;
+  scores: TeamScore[];
+  groupID: string | null;
+  teams: Team[];
+  clientID: string | null;
+  users: User[];
+  suggestionCount: number;
+  numTeams: number;
+}
+
+export type State = ChooseGroup | SignUp | ConfigureGame | Play | GameOver;
 
 export interface GameState {
   state: State;
