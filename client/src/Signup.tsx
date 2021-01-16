b6fe019880d2e5d6dd93e8c266cf2603f7a48ad5
Fred Collman <fredcollman@gmail.com>
2021-01-16 15:53:01 +0000

    Hide socket details away from Signup component

--- client/src/Signup.tsx
+++ client/src/Signup.tsx
@@ -1,15 +1,37 @@
 import { FormEvent } from "react";
+import { Socket } from "socket.io-client";
 import BasicForm from "./BasicForm";
-import useSender from "./useSender";
+import useSocket from "./useSocket";
+
+type Callback<Context, Result> = (ctx: Context) => Result;
+
+interface PerformanceContext {
+  socket: Socket;
+}
+
+const addUser = ({
+  username,
+}: {
+  username: string;
+}): Callback<PerformanceContext, void> =>
+  ({ socket }) => {
+    socket.send("SET_USERNAME", { username });
+  };
+
+const usePerform = <Result extends unknown>() => {
+  const socket = useSocket();
+  return (cb: Callback<PerformanceContext, Result>) =>
+    socket ? cb({ socket }) : null;
+};
 
 const Signup = () => {
-  const setUsername = useSender("SET_USERNAME");
+  const perform = usePerform();
   const handleSubmit = (e: FormEvent) => {
     console.log(e);
     const form = e.target as HTMLFormElement;
     const username = form["username"].value;
     if (username && username.length) {
-      setUsername({ username });
+      perform(addUser({ username }));
     }
   };
   return (
