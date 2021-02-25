3cec5066dac57878dea7889809b87a47e7a863b8
Fred Collman <fredcollman@gmail.com>
2021-02-25 19:02:11 +0000

    Remove websockets from server-side

--- server/main.ts
+++ server/main.ts
@@ -2,10 +2,8 @@ import express from "express";
 import http from "http";
 import path from "path";
 import { v4 } from "uuid";
-import { Server } from "socket.io";
 import low from "lowdb";
 import FileAsync from "lowdb/adapters/FileAsync.js";
-import Client from "./client";
 import apolloServer from "./apollo";
 
 const PORT = 3001;
@@ -13,7 +11,6 @@ const rootDir = path.resolve();
 
 const app = express();
 const httpServer = http.createServer(app);
-const io = new Server(httpServer);
 
 app.use((req, res, next) => {
   const id = v4();
@@ -45,15 +42,5 @@ low(adapter)
       console.log(
         `Subscriptions ready at ws://localhost:${PORT}${apollo.subscriptionsPath}`,
       );
-    });
-
-    io.on("connection", (socket) => {
-      console.log(`[${socket.id}] user connected`);
-      socket.on("disconnect", (reason: string) => {
-        console.log(`[${socket.id}] user disconnected for reason ${reason}`);
-        // TODO remove user from list of players
-      });
-
-      Client.prepare({ io, socket, db });
     });
   });
